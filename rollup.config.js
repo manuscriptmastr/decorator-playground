79f4f5ced795e484c55ab9807da1a8f209dc6bf4
Joshua Martin <the.manuscriptmaster@gmail.com>
2023-10-26 15:10:51 -0400

    Fix dist test issue

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 
+/** @type {import('rollup').RollupOptions} */
 export default {
   input: 'src/index.ts',
   output: {
