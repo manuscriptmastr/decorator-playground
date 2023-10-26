b6e0a8d2dbb39c76544ece98d6d8f8b61edffa01
Joshua Martin <the.manuscriptmaster@gmail.com>
2023-10-26 15:58:39 -0400

    Add tests for customElement and persist

--- src/utils/decorators.test.ts
+++ src/utils/decorators.test.ts
@@ -1,10 +1,9 @@
 jest.mock('lit-html', () => ({
   render: jest.fn(),
 }));
-import { bound } from './decorators';
+import { bound, customElement, persist } from './decorators';
 
 describe('@bound', () => {
-  sessionStorage.getItem('hello');
   it('binds a method', () => {
     class Jungle {
       property = 'banana';
@@ -38,5 +37,109 @@ describe('@bound', () => {
 
     const { getBanana: getBanana2 } = new Jungle2();
     expect(getBanana2()).toBe('banana');
+  });
+});
+
+describe('@customElement(tagName)', () => {
+  it('Registers a custom element with the browser', () => {
+    expect(customElements.get('custom-element')).toBe(undefined);
+
+    @customElement('custom-element')
+    class CustomElement extends HTMLElement {}
+
+    expect(customElements.get('custom-element')).toBeTruthy();
+  });
+});
+
+describe('@persist(storageKey, storage?)', () => {
+  beforeEach(() => {
+    sessionStorage.clear();
+    localStorage.clear();
+  });
+
+  it('initializes session storage with default property', () => {
+    expect(sessionStorage.getItem('test-property')).toBeNull();
+
+    class TestClass {
+      @persist('test-property')
+      accessor showDialog = false;
+    }
+
+    const myClass = new TestClass();
+    expect(myClass.showDialog).toBe(false);
+    expect(sessionStorage.getItem('test-property')).toBe('false');
+  });
+
+  it('initializes value from session storage', () => {
+    sessionStorage.setItem('test-property', 'true');
+
+    class TestClass {
+      @persist('test-property')
+      accessor showDialog = false;
+    }
+
+    const myClass = new TestClass();
+    expect(myClass.showDialog).toBe(true);
+    expect(sessionStorage.getItem('test-property')).toBe('true');
+  });
+
+  it('updates both property and session storage when property is updated and session storage was initialized by default property', () => {
+    class TestClass {
+      @persist('test-property')
+      accessor showDialog = false;
+    }
+
+    const myClass = new TestClass();
+
+    expect(myClass.showDialog).toBe(false);
+    expect(sessionStorage.getItem('test-property')).toBe('false');
+
+    myClass.showDialog = true;
+
+    expect(myClass.showDialog).toBe(true);
+    expect(sessionStorage.getItem('test-property')).toBe('true');
+  });
+
+  it('updates both property and session storage when property is updated and property was initialized by session storage', () => {
+    sessionStorage.setItem('test-property', 'true');
+
+    class TestClass {
+      @persist('test-property')
+      accessor showDialog = false;
+    }
+
+    const myClass = new TestClass();
+
+    expect(myClass.showDialog).toBe(true);
+    expect(sessionStorage.getItem('test-property')).toBe('true');
+
+    myClass.showDialog = false;
+
+    expect(myClass.showDialog).toBe(false);
+    expect(sessionStorage.getItem('test-property')).toBe('false');
+  });
+
+  it('works with local storage', () => {
+    localStorage.setItem('test-property', 'true');
+
+    class TestClass {
+      @persist('test-property', localStorage)
+      accessor showDialog = false;
+    }
+
+    const myClass = new TestClass();
+
+    expect(myClass.showDialog).toBe(true);
+    expect(localStorage.getItem('test-property')).toBe('true');
+
+    myClass.showDialog = false;
+
+    expect(myClass.showDialog).toBe(false);
+    expect(localStorage.getItem('test-property')).toBe('false');
+  });
+
+  afterEach(() => {
+    sessionStorage.clear();
+    localStorage.clear();
   });
 });
